ddeecc0b69cfb4e57933aed0fb525772caf1051d
mgpatil4418 <mgpatil4418@gmail.com>
2025-04-02 12:36:15 +0530

    Add files via upload

---
+++ checkgivennumberpalindrome.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        int reverse = 0;
+        int originalNum = num;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num /= 10;
+        }
+
+        if (originalNum == reverse) {
+            System.out.println(originalNum + " is a palindrome.");
+        } else {
+            System.out.println(originalNum + " is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+}
+
+
